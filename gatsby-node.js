f1384fb6be39d0593fa5a5a9c27d4b2b1cf5fc52
Robin Solanki <web.guru@me.com>
2020-07-08 12:00:37 +0530

    remove alias

--- gatsby-node.js
+++ gatsby-node.js
@@ -64,15 +64,3 @@ exports.createPages = async ({ graphql, actions, reporter }) => {
     })
   })
 }
-
-// For Absolute Imports
-exports.onCreateWebpackConfig = ({ actions }) => {
-  actions.setWebpackConfig({
-    resolve: {
-      // modules: [path.resolve(__dirname, "src"), "node_modules"],
-      alias: {
-        "../components": path.resolve(__dirname, "src/components"),
-      },
-    },
-  })
-}
