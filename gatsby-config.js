1e751cbd0d34c1c2155c6f558a45d1c85f69f60b
Robin Solanki <web.guru@me.com>
2020-07-28 03:40:05 +0530

    Update config file

--- gatsby-config.js
+++ gatsby-config.js
@@ -49,8 +49,12 @@ module.exports = {
     {
       resolve: `gatsby-plugin-gtag`,
       options: {
-        trackingId: "UA-72351877-1",
-        head: false,
+        // your google analytics tracking id
+        trackingId: `UA-72351877-1`,
+        // Puts tracking script in the head instead of the body
+        head: true,
+        // enable ip anonymization
+        anonymize: true,
       },
     },
     {
